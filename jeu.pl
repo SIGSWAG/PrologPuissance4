e195d1b176889252cd899e464e1c847efb815656
unknown <thalepian@IF501-202-01.insa-lyon.fr>
2015-10-07 09:46:32 +0200

    nbLignes & nbColonnes

--- jeu.pl
+++ jeu.pl
@@ -1,22 +1,30 @@
 %%%%%%%%%%%%%%%%% Fichier jeu.pl %%%%%%%%%%%%%%%%%%%
 
+
 % Initialisation du plateau
 
 incr(X,X1):- X1 is X+1.
+decr(X,X1):- X1 is X-1.
 
 
-init:- retractall(case(X,Y,Z)), initLignes(0). %initInterface, play
+init:- initClear, initConstantes, initLignes(0), initTest.
 
-initLignes(5).
+initClear :- retractall(case(X,Y,Z)), retract(nbLignes(C)), retract(nbColonnes(C)).
+
+initConstantes :- assert(nbLignes(5)), assert(nbColonnes(4)).
+
+initTest :- assert(case(1,1,rouge)), assert(case(1,2,rouge)), assert(case(1,3,rouge)), assert(case(1,4,rouge)). %initInterface, play
+
+initLignes(Y) :- nbLignes(Y).
 initLignes(Y):- initColonne(0,Y), incr(Y,Y1), initLignes(Y1).
 
-initColonne(4,Y).
+initColonne(X,Y) :- nbColonnes(X).
 initColonne(X,Y) :- assert(case(X,Y,vide)), incr(X,X1), initColonne(X1,Y).
 
 
 % Play
 
-play (X,Y,J) :- 
+play(X,Y,J).
 
 
 
@@ -24,8 +32,12 @@ play (X,Y,J) :-
 
 
 
-aGagne (X,Y,J).
+gagneColonne(X,Y,J) :- case(X,Y,J), decr(Y,Y1), case(X,Y1,J), decr(Y1,Y2), case(X,Y2,J), decr(Y2,Y3), case(X,Y3,J). %ligne en bas
+
+gagneLigne(X,Y,J,[]).
+gagneLigne(X,Y,J,L) .
+gagneLigne(X,Y,J,L) . 
+% coté, diagonaleS
 
 
 
-gagne(X, X+1, X+2, X+3, Y).
