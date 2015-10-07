0dcf7873cf1a93898df7c088ed6f46cf691c671d
unknown <thalepian@IF501-202-01.insa-lyon.fr>
2015-10-07 10:34:10 +0200

    Initialisation finalisée, on y touche plus les gros

--- jeu.pl
+++ jeu.pl
@@ -1,6 +1,11 @@
 %%%%%%%%%%%%%%%%% Fichier jeu.pl %%%%%%%%%%%%%%%%%%%
 
 
+%%%%%%%%%%%%%%%%% Constantes %%%%%%%%%%%%%%%%%
+
+nbLignes(5).
+nbColonnes(8).
+
 %%%%%%%%%%%%%%%%% Fonctions utiles %%%%%%%%%%%%%%%%%
 
 incr(X,X1):- X1 is X+1.
@@ -11,19 +16,11 @@ decr(X,X1):- X1 is X-1.
 
 
 
-init:- initClear, initConstantes, initLignes(0), initTest.
+init:- initClear.
 
-initClear :- retractall(case(X,Y,Z)), retract(nbLignes(C)), retract(nbColonnes(C)).
+initClear :- retractall(case(X,Y,Z)).
 
-initConstantes :- assert(nbLignes(5)), assert(nbColonnes(4)).
-
-initTest :- assert(case(1,1,rouge)), assert(case(1,2,rouge)), assert(case(1,3,rouge)), assert(case(1,4,rouge)). %initInterface, play
-
-initLignes(Y) :- nbLignes(Y).
-initLignes(Y):- initColonne(0,Y), incr(Y,Y1), initLignes(Y1).
-
-initColonne(X,Y) :- nbColonnes(X).
-initColonne(X,Y) :- assert(case(X,Y,vide)), incr(X,X1), initColonne(X1,Y).
+initTest :- assert(case(1,1,rouge)), assert(case(0,1,rouge)), assert(case(2,1,rouge)), assert(case(3,1,bleu)), assert(case(4,1,rouge)). %initInterface, play
 
 
 
@@ -36,12 +33,20 @@ play(X,Y,J).
 
 
 
-
 gagneColonne(X,Y,J) :- case(X,Y,J), decr(Y,Y1), case(X,Y1,J), decr(Y1,Y2), case(X,Y2,J), decr(Y2,Y3), case(X,Y3,J). %ligne en bas
 
-gagneLigne(X,Y,J) :- gauche(X,Y,J,Rg), droite(X,Y,J,Rd), (Rg+Rd)>3.
+
+gagneLigne(X,Y,J) :- gaucheVerif(X,Y,J,Rg), droiteVerif(X,Y,J,Rd), (Rg+Rd)>4.
 
 
+gaucheVerif(X,Y,J,Rg):- gauche(X,Y,J,0,Rg).
+gauche(X,Y,J,R,R) :- not(case(X,Y,J)). %Jusqu'à la case non J
+gauche(X,Y,J,R,Rg) :- decr(X,X1), incr(R,R1), gauche(X1,Y,J,R1,Rg).
+
+
+droiteVerif(X,Y,J,Rg):- droite(X,Y,J,0,Rg).
+droite(X,Y,J,R,R) :- not(case(X,Y,J)). %Jusqu'à la case non J
+droite(X,Y,J,R,Rg) :- incr(X,X1), incr(R,R1), droite(X1,Y,J,R1,Rg).
 
 % coté, diagonaleS
 
