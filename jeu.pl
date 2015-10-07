f1c4a632ad819529fa7736529ef694f6bd4a5c2a
unknown <ltouzard@IF501-202-02.insa-lyon.fr>
2015-10-07 10:35:37 +0200

    ajout de predicat d'insertion de jeton

--- jeu.pl
+++ jeu.pl
@@ -1,4 +1,6 @@
 %%%%%%%%%%%%%%%%% Fichier jeu.pl %%%%%%%%%%%%%%%%%%%
+nbLignes(5).
+nbColonnes(8).
 
 
 %%%%%%%%%%%%%%%%% Fonctions utiles %%%%%%%%%%%%%%%%%
@@ -11,11 +13,11 @@ decr(X,X1):- X1 is X-1.
 
 
 
-init:- initClear, initConstantes, initLignes(0), initTest.
+init:- initClear, initLignes(0).
 
-initClear :- retractall(case(X,Y,Z)), retract(nbLignes(C)), retract(nbColonnes(C)).
+initClear :- retractall(case(X,Y,Z)).
 
-initConstantes :- assert(nbLignes(5)), assert(nbColonnes(4)).
+
 
 initTest :- assert(case(1,1,rouge)), assert(case(1,2,rouge)), assert(case(1,3,rouge)), assert(case(1,4,rouge)). %initInterface, play
 
@@ -44,6 +46,29 @@ gagneLigne(X,Y,J) :- gauche(X,Y,J,Rg), droite(X,Y,J,Rd), (Rg+Rd)>3.
 
 
 % coté, diagonaleS
+
+
+% placerJeton\3(-Colonne, +Ligne, -Couleur) 
+% insère si possible un jeton dans la colonne donnée
+% retourne la ligne d'insertion, ou no
+placerJeton(X,Y,C) :- coupValide(X), insererJeton(X, Y, C).
+
+% coupValide\1(-Colonne)
+% Vérifie si un jeton est jouable dans cette colonne
+% retourne yes ou no
+coupValide(X) :- nbLignes(NBLIGNES), not(case(X,NBLIGNES,Z)).
+
+% insererJeton\3(-Colonne, +Ligne, -Couleur)
+% Insere, sans vérification, un jeton de la couleur donnée, dans la colonne donnée
+% retourne la ligne d'insertion, 
+insererJeton(X,Y,C) :- calculPositionJeton(X, 0, Y), retract(case(X,Y,vide)), assert(case(X,Y,C)).
+
+% calculPositionJeton\3(+Colonne,+LigneToCheck,-Ligne)
+% calcule la premiere ligne vide d'une colonne
+% retourne l'indice de cette ligne vide
+calculPositionJeton(X,YCheck,YCheck) :- case(X,YCheck,vide), !.
+calculPositionJeton(X,YCheck,Y) :- incr(YCheck, YCheck1), calculPositionJeton(X,YCheck1,Y).
+
 
 
 
