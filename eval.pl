bc0f083a78f0373ff913755baf3ac57f0d06251d
unknown <acoussat@IF501-202-03.insa-lyon.fr>
2015-10-14 11:48:17 +0200

    Détection des puissances 3

--- eval.pl
+++ eval.pl
@@ -20,12 +20,15 @@ evalJeu(Courant,Score) :- evalPosition(Courant,Score).
 %% Prédicats privés %%
 %%%%%%%%%%%%%%%%%%%%%%
 
+% TODO TREEEES IMPORTANT :
+% TOUT BASCULER VERS CASETEST DES QUE MINMAX EST PRET !!!!!!!!!!!!
+
 % evalPosition/2 (+Courant,-Score)
 % Evalue en privilegiant les positions centrales
 % Toujours vrai
 evalPosition(Courant,Score) :- findall(S, evalCases(Courant,S), Scores), sum(Scores, Score).
 
-evalCases(Courant,ScoreCase) :- case(X,Y,_), evalCase(X,Y,Courant,ScoreCase). % basculer vers caseTest !!!!!!!!!!!!
+evalCases(Courant,ScoreCase) :- case(X,Y,_), evalCase(X,Y,Courant,ScoreCase).
 
 evalCase(X,Y,Courant,ScoreCase) :- 
 	nbColonnes(NBCOLONNES), nbLignes(NBLIGNES),
@@ -40,3 +43,21 @@ ponderationJ(_, _, _, -1).
 	
 sum([],0).
 sum([X|Xs],N) :- sum(Xs,N1), N is N1+X.
+
+%%%%%%%%%%%%%%%%%%%%
+
+% evalPuissances3/2(+JoueurCourant,-Score)
+% Évalue en cherchant les positions faisant gagner.
+% Score s'unifie au score de la position.
+evalPuissances3(JoueurCourant,ScoreFinal) :- 
+	findall(S,evalCasesVides(JoueurCourant,S),ScoresCourant), sum(ScoresCourant,ScoreCourant),
+	findall(S,evalCasesVides(AutreJoueur,S),ScoresAutre), sum(ScoresAutre,ScoreAutre),
+	ScoreFinal is ScoreCourant + ScoreAutre.
+
+evalCasesVides(JoueurCourant,ScoreCase) :-
+	nbColonnes(NBCOLONNES), nbLignes(NBLIGNES),
+	between(1,NBCOLONNES,X), between(1,NBLIGNES,Y),
+	caseVide(X,Y),
+	assert(case(X,Y,JoueurCourant)),
+	ScoreCase = (gagne(X,Y,JoueurCourant) -> 1 ; -1),
+	retract(case(X,Y,JoueurCourant).
